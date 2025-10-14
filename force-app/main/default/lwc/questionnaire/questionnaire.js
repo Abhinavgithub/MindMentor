b31c61c1f5c2f163cee33e005137e1d887afba34
Abhinav Singh <adikari.abhinav@gmail.com>
2025-10-14 23:06:06 +0530

    Add access check utility classes and update Questionnaire component functionality

--- force-app/main/default/lwc/questionnaire/questionnaire.js
+++ force-app/main/default/lwc/questionnaire/questionnaire.js
@@ -2,4 +2,7 @@ import { LightningElement } from 'lwc';
 
 export default class Questionnaire extends LightningElement {
     // Minimal component per requirements: display-only header, description, and button.
+    startAssesment(){
+        console.log('Start Assesment Clicked');    
+    }
 }
